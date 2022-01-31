8f359429e36442393a871999ad0ce0102030c546
Chaim <gchaimke@gmail.com>
2022-01-31 18:47:41 +0200

    code reformat, add service

--- Catalog.Api/Program.cs
+++ Catalog.Api/Program.cs
@@ -1,28 +1,25 @@
 using SecuringWebApiUsingApiKey.Middleware;
 using Microsoft.EntityFrameworkCore;
 using Catalog.Api.Repositories;
+using Microsoft.Extensions.Hosting.WindowsServices;
+
+var arg = new WebApplicationOptions
+{
+    Args = args,
+    ContentRootPath = WindowsServiceHelpers.IsWindowsService() ? AppContext.BaseDirectory : default
+};
 
 var builder = WebApplication.CreateBuilder(args);
-// Add services to the container.
+
+builder.Host.UseWindowsService();
 
 builder.Services.AddControllers(options => options.SuppressAsyncSuffixInActionNames = false);
 builder.Services.AddDbContext<SqlDbRepository>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseHttpsRedirection();
-}
-
 app.UseMiddleware<ApiKeyMiddleware>();
 app.UseAuthorization();
 
